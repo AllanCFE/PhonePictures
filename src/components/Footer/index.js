b0d6a0452ec2eaf48cfea9326da09a25254df32d
HydroBR <hydroskaju@gmail.com>
2021-01-21 13:46:58 -0300

    Fix: Logos appear at the Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,13 @@
 import './Footer.css';
 
 export default () => {
+
     return (
         <div className="footer">
             <div className="footer--text">
                 Made by Allan Echeverria | 
-                <img href={process.env.PUBLIC_URL + '/logo192.png'} alt="GitHub"/>
+                <a href="https://github.com/AllanCFE"><img src={process.env.PUBLIC_URL + '/GitHub.png'} alt="GitHub"/></a>
+                <a href="https://www.linkedin.com/in/allan-echeverria/"><img src={process.env.PUBLIC_URL + '/LI.png'} alt="LinkedIn" />  </a>
             </div>
         </div>
     );
